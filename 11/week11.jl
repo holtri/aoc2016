c998adb4be082c15c894eef7a196a242ef06cd76
Holger <holger.trittenbach@gmail.com>
2017-07-30 17:46:15 +0200

    week 11

--- 11/week11.jl
+++ 11/week11.jl
@@ -9,14 +9,39 @@ function isSafe(floor::Array{<:Integer,1})
   return safe
 end
 
+function isSafe(state::Matrix)
+  for row in indices(state,1)
+    !isSafe(state[row, :]) && return false
+  end
+  return true
+end
+
 @testset "isSafe Tests" begin
-         @test !isSafe([1,1,0,0,1,1,0,0,0,1])
-         @test isSafe([1,1,0,0,1,1,0,0,1,0])
-         @test isSafe([1,1,0,0,1,1,0,0,1,1])
-         @test isSafe([0,0,0,0,0,0,0,0,0,0])
-         @test isSafe([1,0,1,0,0,0,0,0,0,0])
-         @test isSafe([1,1,1,0,0,0,0,0,0,0])
-         @test !isSafe([1,1,0,1,0,0,0,0,0,0])
+         @testset "single floor" begin
+           @test !isSafe([1,1,0,0,1,1,0,0,0,1])
+           @test isSafe([1,1,0,0,1,1,0,0,1,0])
+           @test isSafe([1,1,0,0,1,1,0,0,1,1])
+           @test isSafe([0,0,0,0,0,0,0,0,0,0])
+           @test isSafe([1,0,1,0,0,0,0,0,0,0])
+           @test isSafe([1,1,1,0,0,0,0,0,0,0])
+           @test !isSafe([1,1,0,1,0,0,0,0,0,0])
+         end
+         @testset "state" begin
+          @test begin
+            teststate = [0 0 0 0 0 0 0 0 0 0;
+                    0 0 0 0 0 1 0 0 0 0;
+                    0 0 0 0 1 0 1 1 1 1;
+                    1 1 1 1 0 0 0 0 0 0]
+            isSafe(teststate)
+          end
+          @test begin
+            teststate = [0 0 0 0 0 0 0 0 0 0;
+                    1 1 0 0 0 1 0 0 0 0;
+                    0 0 0 0 1 0 1 1 1 1;
+                    0 0 1 1 0 0 0 0 0 0]
+            !isSafe(teststate)
+          end
+       end
 end
 
 function moveElevator(state, rowIndexA, rowIndexB, colIndices)
@@ -27,31 +52,31 @@ function moveElevator(state, rowIndexA, rowIndexB, colIndices)
 end
 
 @testset "moveElevator test" begin
-teststate = [0 0 0 0 0 0 0 0 0 0;
-         0 0 0 0 0 1 0 0 0 0;
-         0 0 0 0 1 0 1 1 1 1;
-         1 1 1 1 0 0 0 0 0 0]
+  teststate = [0 0 0 0 0 0 0 0 0 0;
+          0 0 0 0 0 1 0 0 0 0;
+          0 0 0 0 1 0 1 1 1 1;
+          1 1 1 1 0 0 0 0 0 0]
   @test begin
-           before = teststate
-           after = moveElevator(teststate, 3, 4, [1,2])
-           after = moveElevator(after, 3, 4, [1,2])
-           before == after
+    before = teststate
+    after = moveElevator(teststate, 3, 4, [1,2])
+    after = moveElevator(after, 3, 4, [1,2])
+    before == after
   end
   @test begin
-           after = moveElevator(teststate, 3, 4, [9,10])
-           after[3,9] == 0 && after[3,10] == 0 && after[4,9] == 1 & after[4,10] == 1
+    after = moveElevator(teststate, 3, 4, [9,10])
+    after[3,9] == 0 && after[3,10] == 0 && after[4,9] == 1 & after[4,10] == 1
   end
   @test begin
-         before = teststate
-         moveElevator(teststate, 3, 4, [9,10])
-         before == teststate
+    before = teststate
+    moveElevator(teststate, 3, 4, [9,10])
+    before == teststate
   end
 end
 
 function adjacentFloors(x)
-         x == 1 && return [2]
-         (x == 2 || x == 3) && return [x-1, x+1]
-         x == 4 && return [3]
+  x == 1 && return [2]
+  (x == 2 || x == 3) && return [x-1, x+1]
+  x == 4 && return [3]
 end
 
 macro pretty(m)
@@ -60,34 +85,77 @@ macro pretty(m)
   end)
 end
 
-state = [0 0 0 0 0 0 0 0 0 0;
-         0 0 0 0 0 1 0 0 0 0;
-         0 0 0 0 1 0 1 1 1 1;
-         1 1 1 1 0 0 0 0 0 0]
+function swapPairs(state, pairA, pairB)
+  tmp = copy(state)
+  tmp[:, pairB] = state[:, pairA]
+  tmp[:, pairA] = state[:, pairB]
+  tmp
+end
 
-searchFront = [(4, state)]
-nextLevelCandidates = []
+@testset "swap test" begin
+  teststate = [0 0 0 0 0 0 0 0 0 0;
+          0 0 0 0 0 1 0 0 0 0;
+          0 0 0 0 1 0 1 1 1 1;
+          1 1 1 1 0 0 0 0 0 0]
+  @test begin
+    expected = [0 0 0 0 0 0 0 0 0 0;
+            0 0 0 0 0 1 0 0 0 0;
+            1 1 0 0 1 0 1 1 0 0;
+            0 0 1 1 0 0 0 0 1 1]
+    actual = swapPairs(teststate, [1,2], [9,10])
+    actual == expected
+  end
+  @test begin
+    expected = copy(teststate)
+    swapPairs(teststate, [1,2], [9,10])
+    expected == teststate
+  end
+end
 
-steps = 0
-while steps < 2
-  for state in searchFront
-    elevatorPositon = state[1]
-    materialPositions = state[2]
-    # candidate = Matrix(0,0) # have to declare this, otherwise the macro can't find it in scope
-    for c in subsets(1:10,2) # all possible actions
-      if sum(materialPositions[elevatorPosition, c] ) > 0 # at least one part must be moved
-        for ap in adjacentFloors(elevatorPosition)
-          candidate = moveElevator(materialPositions, elevatorPosition, ap, c)
-          println(ap)
-          push!(nextLevelCandidates, (ap, candidate))
+function addVisited!(visitedStates, elevatorPosition, state)
+  for c in subsets([1,3,5,7,9], 2)
+      push!(visitedStates, hash((elevatorPosition, swapPairs(state, [c[1], c[1]+1], [c[2], c[2]+1]))))
+  end
+end
+
+function solve()
+  initialMaterialPositions = [0 0 0 0 0 0 0 0 0 0;
+           0 0 0 0 0 1 0 0 0 0;
+           0 0 0 0 1 0 1 1 1 1;
+           1 1 1 1 0 0 0 0 0 0]
+
+  searchFront = [(4, initialMaterialPositions)]
+  nextLevelCandidates = []
+  visitedStates = Set()
+  steps = 1
+  solution = false
+  maxSoFar = 0
+  while steps < 40 && !solution
+    j = 0
+
+    for state in searchFront
+      j % 1000 == 0 && println("j: $j")
+      j+=1
+      elevatorPosition = state[1]
+      materialPositions = state[2]
+      candidate = Matrix(0,0)
+      for c in subsets(1:10,2) # all possible actions
+        if sum(materialPositions[elevatorPosition, c] ) > 0 # at least one part must be moved
+          for ap in adjacentFloors(elevatorPosition)
+            candidate = moveElevator(materialPositions, elevatorPosition, ap, c)
+            !in(hash((ap, candidate)), visitedStates) && isSafe(candidate) && (push!(nextLevelCandidates, (ap, candidate)); addVisited!(visitedStates, ap, candidate))
+            sum(candidate[1, :]) == 10 && (println("Solution Found!"); solution = true)
+            sum(candidate[1, :]) > maxSoFar && (maxSoFar = sum(candidate[1, :]); println("up at highest floor: $(sum(candidate[1, :]))"))
+          end
         end
       end
     end
+    searchFront = nextLevelCandidates
+    println("Search Front Size = $(length(searchFront))")
+    nextLevelCandidates = []
+    steps += 1
   end
-  searchFront = nextLevelCandidates
-  steps += 1
 end
 
-
+solve()
 # candidate = moveElevator(state, elevatorPosition, 3, [1,2])
-@pretty candidate
