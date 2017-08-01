ddca7ea8e3d387b64b46ccb64822978945222506
Holger <holger.trittenbach@gmail.com>
2017-08-01 21:22:11 +0200

    change to parse input only once

--- 12/week12.jl
+++ 12/week12.jl
@@ -1,25 +1,58 @@
-input = readlines(open("input.txt"))
+struct Command{A<:Union{Int64, Char}, B<:Union{Int64, Char}}
+  command::String
+  arg1::A
+  arg2::B
+end
 
-function solve(input)
-  i=1
-  register = fill(0, 4)
-  # register[3] = 1
+function parseInput(filename)
+  input = readlines(open(filename))
+  commands = Vector{Command}()
+
+  for line in input
+    l = split(line)
+    if isa(parse(l[2]), Number)
+        arg1 = parse(Int64, l[2])
+        type1 = Int64
+    else
+      arg1 = l[2][1]
+      type1 = Char
+    end
+    arg2 = 0
+    type2 = Int64
+    if length(l) > 2
+      if isa(parse(l[3]), Number)
+          arg2 = parse(Int64, l[3])
+          type2 = Int64
+      else
+        arg2 = l[3][1]
+        type2 = Char
+      end
+    end
+    push!(commands, Command{type1,type2}(l[1],arg1,arg2))
+  end
+  return commands
+end
+
+function solve(input::Vector{Command})
+  i::Int64 = 1
+  register::Vector{Int64} = fill(0, 4)
+  register[3] = 1
+
   counter = 1
   while i <= length(input)
     counter +=1
-    # counter % 10000 == 0 && println("counter: $counter i: $i c: $(input[i])")
-    c = split(input[i])
+    # counter % 10000 == 0 &&  println("counter: $counter i: $i c: $(input[i])")
 
-    if c[1] == "inc"
-      register[Int(c[2][1])-96] +=1
-    elseif c[1] == "dec"
-      register[Int(c[2][1])-96] -=1
-    elseif c[1] == "cpy"
-        v = isa(parse(c[2]), Number) ? parse(Int, c[2]) : register[Int(c[2][1]) - 96]
-        register[Int(c[3][1]) - 96] = v
+    if input[i].command == "inc"
+      register[Int64(input[i].arg1) - 96] += 1
+    elseif input[i].command == "dec"
+      register[Int64(input[i].arg1) - 96] -= 1
+    elseif input[i].command == "cpy"
+        v = isa(input[i].arg1, Number) ? input[i].arg1 : register[Int64(input[i].arg1) - 96]
+        register[Int64(input[i].arg2) - 96] = v
     else
-       if isa(parse(c[2]), Number) ? parse(Int, c[2]) != 0 : register[Int(c[2][1]) - 96] != 0
-         i += isa(parse(c[3]), Number) ? parse(Int, c[3]) : register[Int(c[3][1]) - 96]
+       if isa(input[i].arg1, Number) ? input[i].arg1 != 0 : register[Int64(input[i].arg1) - 96] != 0
+         i += isa(input[i].arg2, Number) ? input[i].arg2 : register[Int64(input[i].arg2) - 96]
          continue
        end
     end
@@ -28,4 +61,5 @@ function solve(input)
   register
 end
 
+input = parseInput("input.txt")
 @time solve(input)
