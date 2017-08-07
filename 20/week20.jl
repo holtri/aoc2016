35eedac47f804cb1b4d3a3c88ec62b8df0be45a0
Holger <holger.trittenbach@gmail.com>
2017-08-07 23:47:19 +0200

    week 20

---
+++ 20/week20.jl
@@ -0,0 +1,43 @@
+macro readinput(x)
+  :(readlines(open(joinpath(@__DIR__, $x))))
+end
+
+input = map(x -> parse.(Int, split(x, "-")), @readinput "input.txt")
+
+inInterval(x::Int, interval::Vector{Int}) = x >= interval[1] && x <= interval[2]
+
+function inAnyInterval(x, intervals)
+  for interval in intervals
+    inInterval(x, interval) && return true
+  end
+  return false
+end
+
+function solve()
+  value = 1
+  intervals = sort(input, lt = (x,y)-> x[1] < y[1])
+  while value < 4294967295
+    !inAnyInterval(value, input) && break
+    value+=1
+  end
+  value
+end
+@time solve()
+
+function solve2()
+  intervals = sort(input, lt = (x,y)-> x[1] < y[1])
+  value = 0
+  i = 0
+  while i <= 4294967295
+    if inInterval(i,intervals[1])
+      # println("interval: $(intervals[1])")
+      i = intervals[1][2] + 1
+      intervals = [x for x in intervals if x[2] > intervals[1][2]]
+    else
+      value +=1
+      i +=1
+    end
+  end
+  value
+end
+@time solve2()
